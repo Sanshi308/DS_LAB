cdc59295e903b22a1b0f0f51f5db69130cc4a40f
Sanshi <dsanshi0629@gmail.com>
2025-08-27 23:07:45 +0530

    Q6.cpp

---
+++ Assignment2/Q6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct Element {
+    int row, col, val;
+};
+
+int main() {
+    // Example: Matrix 3x3 with only few non-zero values
+    Element mat[3] = {{0,1,5}, {1,0,8}, {2,2,9}};
+
+    // (a) Transpose
+    cout << "Transpose Triplet: ";
+    for(int i=0;i<3;i++)
+        cout << "("<<mat[i].col<<","<<mat[i].row<<","<<mat[i].val<<") ";
+    cout << endl;
+    return 0;
+}
