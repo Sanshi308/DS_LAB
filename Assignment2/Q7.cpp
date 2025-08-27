a2734b82cd4feb8b4b95a18b8e58fa03c1313b57
Sanshi <dsanshi0629@gmail.com>
2025-08-27 23:18:03 +0530

    Q7.cpp

--- Assignment2/Q7.cpp
+++ Assignment2/Q7.cpp
@@ -1,3 +1,7 @@
+7) Let A[1 …. n] be an array of n real numbers. A pair (A[i], A[j ]) is said to be an
+inversion if these numbers are out of order, i.e., i < j but A[i]>A[j ]. Write a program to
+count the number of inversions in an array.
+ans:-
 #include <iostream>
 using namespace std;
 
