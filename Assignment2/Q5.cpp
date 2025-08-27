dd8fc459bc078fc940c0422fdd7f0836fb86e4be
Sanshi <dsanshi0629@gmail.com>
2025-08-27 23:13:59 +0530

    Q5.cpp

--- Assignment2/Q5.cpp
+++ Assignment2/Q5.cpp
@@ -1,3 +1,12 @@
+Space required to store any two-dimensional array is 𝑛𝑢𝑚𝑏𝑒𝑟 𝑜ƒ 𝑟𝑜𝑤𝑠 × 𝑛𝑢𝑚𝑏𝑒𝑟 𝑜ƒ
+𝑐𝑜𝑙𝑢𝑚𝑛𝑠. Assuming an array is used to store elements of the following matrices,
+implement an efficient way that reduces the space requirement.
+(a) Diagonal Matrix.
+(b) Tri-diagonal Matrix.
+(c) Lower triangular Matrix.
+(d) Upper triangular Matrix.
+(e) Symmetric Matrix
+ans:-
 #include <iostream>
 using namespace std;
 
@@ -15,3 +24,5 @@ int main() {
     }
     return 0;
 }
+
+
