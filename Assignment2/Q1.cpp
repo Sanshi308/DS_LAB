e5d40163d5b29313b5a2e8f6856a90614fe6558c
Sanshi <dsanshi0629@gmail.com>
2025-08-31 20:37:53 +0530

    Q1.cpp

--- Assignment2/Q1.cpp
+++ Assignment2/Q1.cpp
@@ -1,39 +1,53 @@
 1) Implement the binary search algorithm regarded as a fast search algorithm
 with run-time complexity of Ο(log n) in comparison to the Linear Search.
 ans:- 
+
 #include <iostream>
 using namespace std;
 
-
-int linearSearch(int arr[], int n, int key) {
-    for(int i = 0; i < n; i++) {
-        if(arr[i] == key)
-            return i; 
-    }
-    return -1; 
-}
-
-
 int binarySearch(int arr[], int n, int key) {
     int low = 0, high = n - 1;
-    while(low <= high) {
-        int mid = (low + high) / 2;
-        if(arr[mid] == key)
-            return mid; 
-        else if(arr[mid] < key)
-            low = mid + 1; 
-        else
-            high = mid - 1; 
+
+    while (low <= high) {
+        int mid = (low + high) / 2;  // middle index
+
+        if (arr[mid] == key) {
+            return mid;   // element found
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;   // search in right half
+        }
+        else {
+            high = mid - 1;  // search in left half
+        }
     }
-    return -1;
+    return -1;  // not found
 }
 
 int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = 5;
-    int key = 30;
+    int n, key;
+    cout << "Enter size of array: ";
+    cin >> n;
 
-    cout << "Linear Search result: " << linearSearch(arr, n, key) << endl;
-    cout << "Binary Search result: " << binarySearch(arr, n, key) << endl;
+    int arr[n];
+    cout << "Enter elements in sorted order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found." << endl;
+
     return 0;
 }
+
+
+
+
